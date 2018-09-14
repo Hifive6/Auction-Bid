639956f4175e2afd6bf054a42718943ace43dea9
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-09-14 15:05:01 -0400

    got a connection to the db

--- auction.js
+++ auction.js
@@ -11,24 +11,58 @@ var connection = mysql.createConnection({
   
   connection.connect(function(err) {
     if (err) throw err;
+    createProduct();
     // console.log("connected as id " + connection.threadId);
-    connection.end();
+    //connection.end();
   });
-inquirer.prompt([
-   
+
+
+    inquirer.prompt([
     {
         type: "list",
         name: "userChoice",
         message: "What would you like to do?",
         choices: ["Post", "Bid"],
       }
-   
+
+    
+    
+    
 ]).then(function(user){
     if(user.userChoice === "Post"){
-        console.log("Post was chosen!");
+        
+        postItem();
+    
+        
     }else{
         console.log("Bid was chosen!")
     }
-})
+    
+
+});
+function postItem(){
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What would you like to Post?"
+        }
+    ])
+    
+}
+
+function createProduct(){
+    console.log("Inserting a new product...\n");
+    var query = connection.query(
+        "INSERT INTO auction SET ?",
+        {
+            
+        },
+        function(err, res){
+            console.log(res.affectedRows + "product inserted!\n");
+        }
+    );
+    console.log(query.sql);
+}
 
   
